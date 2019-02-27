55dfe9bd11492119089b78d8eba220ba2ad70f90
Annie Phan <anniephan.atp@gmail.com>
2019-02-27 05:03:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
-def speak_to_grandma
-  phrase = 'I LOVE YOU GRANDMA!'
-  if phrase == 'I LOVE YOU GRANDMA!'
+phrase = 'I LOVE YOU GRANDMA!'
+
+if phrase == 'I LOVE YOU GRANDMA!'
     puts 'I LOVE YOU TOO PUMPKIN!'
   else
     puts 'HUH, SPEAK UP SONNY!'
