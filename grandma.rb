eccadd29ec190d12a7599a4a773989ae35af7746
Annie Phan <anniephan.atp@gmail.com>
2019-02-27 05:14:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(string)
 
 if string != .upcase
-    puts 'HUH?! SPEAK UP, SONNY!'
+    return 'HUH?! SPEAK UP, SONNY!'
   elsif string.upcase
-    puts 'NO, NOT SINCE 1938!'
-  elsif string == 'I LOVE YOU GRANDMA!'
-    puts 'I LOVE YOU TOO PUMPKIN!'
+    return 'NO, NOT SINCE 1938!'
+  else string == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
 end
 end
