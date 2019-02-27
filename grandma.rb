ef305637e0aea64a115c3038eb51500c221261ec
Annie Phan <anniephan.atp@gmail.com>
2019-02-27 05:09:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,8 @@
-phrase = 'I LOVE YOU GRANDMA!'
+def speak_to_grandma(string)
 
-if phrase == 'I LOVE YOU GRANDMA!'
-    puts 'I LOVE YOU TOO PUMPKIN!'
-  elsif phrase == .upcase
+if string == .downcase
+    puts 'HUH?! SPEAK UP, SONNY!'
+  elsif string.upcase
     puts 'NO, NOT SINCE 1938!'
-  else
-    puts 'HUH, SPEAK UP SONNY!'
+end
 end
