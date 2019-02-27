faac5ebf87d4588d49f0af67f861ec832f322055
Annie Phan <anniephan.atp@gmail.com>
2019-02-27 04:50:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,11 @@
+def speak_to_grandma
+  name = "grandma"
+  if name == "grandma"
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif name == "GRANDMA"
+  puts "NO, NOT SINCE 1938!"
+elsif name == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
