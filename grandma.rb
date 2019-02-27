69c391a36fb61aa7bf7654dfe03f3b2b2ba8b97f
Annie Phan <anniephan.atp@gmail.com>
2019-02-27 05:12:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,5 +4,7 @@ if string != .upcase
     puts 'HUH?! SPEAK UP, SONNY!'
   elsif string.upcase
     puts 'NO, NOT SINCE 1938!'
+  elsif string == 'I LOVE YOU GRANDMA!'
+    puts 'I LOVE YOU TOO PUMPKIN!'
 end
 end
