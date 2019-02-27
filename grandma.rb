7868dce2f2d76e8611b9049b30fb92b5c3ff3d4c
Annie Phan <anniephan.atp@gmail.com>
2019-02-27 05:19:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(string)
 
-if string == string.downcase
+if string != string.upcase
     return 'HUH?! SPEAK UP, SONNY!'
 elsif string == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
